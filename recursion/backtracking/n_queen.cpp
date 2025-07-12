d6aeed8cc57d9047f7ba6a6329159cba38379c58
Madhavkalia076 <madhavkalia054@gmail.com>
2025-07-12 13:09:16 +0530

    12/07/2025

---
+++ recursion/backtracking/n_queen.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve(int col, vector<string> &board, vector<vector<string>> &ans, vector<int> &leftrow, vector<int> &upperdiagonal, vector<int> &lowerdiagonal, int n)
+{
+  if (col == n)
+  {
+    ans.push_back(board);
+    return;
+  }
+  for (int row = 0; row < n; row++)
+  {
+    if (leftrow[row] == 0 && lowerdiagonal[row + col] == 0 && upperdiagonal[n - 1 + col - row] == 0)
+    {
+      board[row][col] = 'Q';
+      leftrow[row] = 1;
+      lowerdiagonal[row + col] = 1;
+      upperdiagonal[n - 1 + col - row] = 1;
+      solve(col + 1, board, ans, leftrow, upperdiagonal, lowerdiagonal, n);
+      board[row][col] = '.';
+      leftrow[row] = 0;
+      upperdiagonal[n - 1 + col - row] = 0;
+      lowerdiagonal[row + col] = 0;
+    }
+  }
+}
+vector<vector<string>> Nqueen(int x)
+{
+  vector<vector<string>> ans;
+  vector<string> board(x);
+  string s(x, '.');
+  for (int i = 0; i < x; i++)
+  {
+    board[i] = s;
+  }
+  vector<int> leftrow(x, 0);
+  vector<int> upperdiagonal(2 * (x - 1), 0);
+  vector<int> lowerdiagonal(2 * (x - 1), 0);
+  solve(0, board, ans, leftrow, upperdiagonal, lowerdiagonal, x);
+  return ans;
+}
+int main()
+{
+  int x;
+  cout << "enter the value of x: ";
+  cin >> x;
+  vector<vector<string>> s = Nqueen(x);
+  for (auto i : s)
+  {
+    for (auto j : i)
+    {
+      cout << j << " ";
+    }
+    cout << endl;
+  }
+  return 0;
+}
