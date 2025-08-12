8bea863dd9b3da95c94323200c5e5490d53efb94
Madhavkalia076 <madhavkalia054@gmail.com>
2025-08-12 22:45:15 +0530

    12/8/2025

--- DP/count_subset_with_target.cpp
+++ DP/count_subset_with_target.cpp
@@ -18,7 +18,7 @@ using namespace std;
 
 int main()
 {
-  vector<int> arr = {1, 2, 3, 5, 8, 10, 11, 12};
+  vector<int> arr = {1, 2, 3, 5, 8, 10, 11, 12, 13};
   int target = 13;
   int n = arr.size();
   vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
